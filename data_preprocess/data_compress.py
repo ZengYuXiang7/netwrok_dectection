afa15db947168e289819de7fc632bdb31734a5c2
sillage-hjj <1069587533@qq.com>
2024-12-09 17:11:26 +0800

    test commit

--- data_preprocess/data_compress.py
+++ data_preprocess/data_compress.py
@@ -1,4 +1,55 @@
 from jenkspy import JenksNaturalBreaks
 import numpy as np
 import csv
+from itertools import combinations
+csv_dir = './datasets/MedBIoT/csv/10s/bashlite_leg_10s.csv'
 
+data = []
+timestamps = []
+
+with open(csv_dir, 'r') as csv_file :
+    reader = csv.reader(csv_file)
+    for line in reader:
+        data.append(int(line[-1]))
+        timestamps.append(line[0])
+
+m = 4   #类别数
+GVF = []
+same = []
+A = list(set(data))
+A.sort()
+print(len(A))
+if m <= 1 or m >= len(A): print(f'类别数m应大于等于1小于等于{len(A)}')
+else:
+    #列表劈分函数split
+    def split(A,B):
+        list_tmp = []
+        B.insert(0,0)
+        B.append(len(A))
+        for i in range(len(B)-1):
+            list_tmp.append(A[B[i]:B[i+1]])
+        return list_tmp
+    #列表偏差平方函数SDAM
+    def SDAM(A): return sum((i - np.mean(A)) ** 2 for i in A)
+    #枚举所有插孔方式
+    kong = list(combinations(range(1, len(A)), m-1))
+    for i in kong:
+        GVF.append(1-(sum(SDAM(j) for j in split(A, list(i))) / SDAM(A)))
+    for i in range(len(GVF)):
+        if GVF[i] == max(GVF) : same.append(i)
+        else: continue
+    print(A)
+    for k in range(len(same)):
+        interval = split(A, list(kong[same[k]]))
+        print(f'第{str(k+1)}种分类方式的分类区间为')
+        for i in range(len(interval)):
+            print(f'第{str(i+1)}类：', end='\t')
+            if len(interval) == 1:
+                print(f'单点{str(interval[i][0])}自成一类')
+            else:
+                if i == 0:
+                    print(f'[{str(interval[i][0])}, {str(interval[i][-1])}]')
+                else:
+                    print(f'[{str(interval[i-1][-1])}, {str(interval[i][-1])}]')
+                    
+    
---
+++ data_preprocess/data_generator.py
@@ -0,0 +1,98 @@
+import os
+import glob
+import pyshark
+import csv
+import logging
+
+time_interval = 10
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+log = logging.getLogger(__name__)
+
+def pcap_to_csv(pcap_file, csv_file):
+    """
+    将pcap文件转换为csv文件,提取五元组、数据包真实长度和时间戳
+    """
+    # 创建CSV文件并写入表头
+    with open(csv_file, 'w', newline='') as f:
+        writer = csv.writer(f)
+        
+        # 使用pyshark读取pcap文件
+        log.debug('before cap')
+        cap = pyshark.FileCapture(pcap_file, display_filter='ip')  # 使用过滤器提取IP层的数据包
+        #cap = pyshark.FileCapture(pcap_file)  # 使用过滤器提取IP层的数据包
+        log.debug('after cap')
+        first_timestamp = None #第一个数据包的时间戳
+        
+        for packet in cap:
+            # 提取五元组信息
+            try:
+                # 获取IP层的信息
+                timestamp = packet.sniff_time  # 获取时间戳
+                source_ip = packet.ip.src  # 源IP
+                destination_ip = packet.ip.dst  # 目标IP
+                
+                # 对于TCP/UDP协议，提取源端口和目标端口
+                if 'TCP' in packet:
+                    source_port = packet.tcp.srcport  # 源端口
+                    destination_port = packet.tcp.dstport  # 目标端口
+                    protocol = 'TCP'
+                elif 'UDP' in packet:
+                    source_port = packet.udp.srcport  # 源端口
+                    destination_port = packet.udp.dstport  # 目标端口
+                    protocol = 'UDP'
+                else:
+                    source_port = destination_port = 'N/A'  # 如果不是TCP/UDP，端口设置为N/A
+                    protocol = packet.highest_layer  # 使用最高层协议（如ICMP）
+
+                # 获取数据包的真实长度
+                length = packet.length  # 数据包的总长度
+                
+                if first_timestamp is None:
+                    first_timestamp = timestamp  # 第一个数据包的时间戳
+                
+                # 计算与第一个包的时间戳差值（单位：秒）
+                time_diff = (timestamp - first_timestamp).total_seconds()
+                if time_diff > time_interval: 
+                    break
+                
+                # 写入数据到CSV
+                writer.writerow([time_diff, source_ip, destination_ip, source_port, destination_port, protocol, length])
+            except AttributeError:
+                # 如果包中缺少某些属性（如非IP包），跳过
+                continue
+        cap.close()
+
+def process_pcap_files_in_directory(input_dir, csv_dir):
+    """
+    遍历指定文件夹中的所有pcap文件，并将其转换为csv文件
+    """
+    input_pattern = os.path.join(input_dir, '*.pcap')  # 匹配所有.pcap文件
+    pcap_files = glob.glob(input_pattern)  # 获取所有匹配的pcap文件
+
+    # with concurrent.futures.ThreadPoolExecutor() as executor:
+    #     futures = []
+    for pcap_file in pcap_files:
+        print(f"Processing pcap file: {pcap_file}")
+        output_dir = csv_dir + f'{time_interval}s/'
+        if not os.path.exists(output_dir):
+            os.mkdir(output_dir)
+        
+        # 获取不带扩展名的文件名
+        base_name = os.path.splitext(os.path.basename(pcap_file))[0]
+        
+        # 构建csv文件的输出路径（与pcap文件同名，但扩展名为.csv）
+        csv_file = os.path.join(output_dir, base_name + '_' + str(time_interval) + 's' + '.csv')
+
+        # 将pcap文件转换为csv文件
+        pcap_to_csv(pcap_file, csv_file)
+        # futures.append(executor.submit(pcap_to_csv, pcap_file, csv_file))
+        
+        print(f"Converted pcap file to csv: {csv_file}")
+        # concurrent.futures.wait(futures)
+
+# 将文件夹`././datasets/MedBIoT_pcap/`中的所有pcap文件转换为csv文件
+input_directory = './datasets/MedBIoT_pcap/'    # 输入pcap文件夹路径
+output_directory = './datasets/MedBIoT/csv/'    # 输出csv文件夹路径
+process_pcap_files_in_directory(input_directory, output_directory)
